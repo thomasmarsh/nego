b9855e35d280c86e1053f5d193e603dbcf039f02
Thomas Marsh <thomas.marsh2@gmail.com>
2024-01-25 12:55:37 -0500

    Remove rustyline

--- bin/demo.rs
+++ bin/demo.rs
@@ -3,12 +3,11 @@ use nego::{agent, core};
 #[macro_use]
 extern crate log;
 
-fn main() -> rustyline::Result<()> {
+fn main() {
     pretty_env_logger::init();
 
     info!("initializing ray LUT");
     core::ray::Rays::build_lut();
 
     agent::demo_minimax();
-    Ok(())
 }
